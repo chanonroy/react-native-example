2a5c1ebaf8b934ded9b211c529de8ac4e296a2cf
Chanon Roy <chanonroy@gmail.com>
2020-03-09 21:11:03 +0000

    feat: add header component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Header />
+      <View style={styles.container}>
+        <Text> Hello World </Text>
+      </View>
     </View>
   );
 }
@@ -12,6 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 80,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
