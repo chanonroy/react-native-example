408d33b8a203a8e5596fc1750143c048718a70df
Chanon Roy <chanonroy@gmail.com>
2020-03-23 16:30:15 +0000

    feat: add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/header';
+import Home from './screens/home';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-semibold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+});
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  return (
-    <View>
-      <Header />
-      <View style={styles.container}>
-        <Text> Hello World </Text>
-      </View>
-    </View>
-  );
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts} 
+        onFinish={() => setFontsLoaded(true)} 
+      />
+    )
+  }
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 80,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
